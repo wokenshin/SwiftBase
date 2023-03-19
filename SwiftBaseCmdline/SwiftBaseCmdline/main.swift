e52da25ea2946d47f45555d745a9f9322a465989
HanKibou <wokenshin@vip.qq.com>
2023-03-19 22:46:42 +0800

    swift基础部分快速学习

--- SwiftBaseCmdline/SwiftBaseCmdline/main.swift
+++ SwiftBaseCmdline/SwiftBaseCmdline/main.swift
@@ -122,4 +122,171 @@ func dic2() {
 }
 
 //dic1()
-dic2()
+//dic2()
+
+//面向对象
+class Person {//注意 这里是定义类 左边没有圆括号
+    //成员变量
+    var name: String = ""//在定义属性后必须要给一个初始值、如果不给定、必须在构造函数中指定初始值
+    var age: Int = 0
+    
+    //定义属性（就是重写 成员的 set get方法）【新知识】
+    var Age:Int {
+        set(value){
+            if value > 150 {
+                self.age = 150
+            } else if value < 0 {
+                self.age = 0
+            } else {
+                self.age = value
+            }
+        }
+        get{
+            return self.age
+        }
+    }
+    
+    //类变量
+    static var like:String = "girl"
+    
+    //闭包
+    var func1:()->Void = {
+        print("我是一个无参数、无返回值的闭包")
+    }
+    
+    var func2:(String, Int)->String = {str, num in // 注意这里用 in 隔开 左边是形式参数
+        return "我是一个带参数、带返回值的闭包：\(str), \(num)"
+    }
+    
+    
+    //成员方法
+    func sayHello() {
+        print("你好， 我的名字叫\(name), 我今年\(age)岁了。")
+    }
+    
+    //类方法
+    static func sleep() {
+        print("我想睡觉了")
+    }
+}
+
+let p = Person()
+//p.sayHello()
+p.name = "kibou"
+//p.sayHello()
+
+p.Age = 160//注意 这里是p.Age  不是p.age
+//p.sayHello()
+//print(Person.like)
+//Person.sleep()
+
+//p.func1()//调用闭包
+let str = p.func2("kenshin", 20)
+//print(str)
+
+//引用传递函数定义
+
+//下面的写法会报错、因为num1 和 num2 是不可变参数 要想可变 必须在 参数后面添加 inout 修饰符
+//func swap(num1:Int, num2:Int) {
+//    let tmp = num1
+//    num1 = num2;
+//    num2 = tmp;
+//}
+
+func swap (num1:inout Int, num2:inout Int) {//注意 参数修饰之间只有一个冒号:
+    let tmp = num1;
+    num1 = num2;
+    num2 = tmp;
+}
+
+var a = 10;
+var b = 20;
+//print("修改前 a:\(a),b:\(b)")
+swap(num1: &a, num2: &b)//这里调用swap的时候会带上&符、其实就是【引用传递】的意思
+//print("修改后 a:\(a),b:\(b)")
+
+//泛型 定义函数 这样的话就可以交换两个相同的任意类型的变量了
+func swap2<T> (num1:inout T, num2:inout T) {//注意！ 这里用 T 代表任意类型 即 泛型
+    let tmp = num1;
+    num1 = num2;
+    num2 = tmp
+}
+var str1 = "kenshin"
+var str2 = "kibou"
+//print("修改前 str1:\(str1), str2:\(str2)")
+swap2(num1: &str1, num2: &str2)
+//print("修改前 str1:\(str1), str2:\(str2)")
+
+//初始化
+class Person2 {
+    var name: String = "哈哈"
+    var age: Int
+    
+    //如果声明变量的时候不给默认值、那么初始化方法里面一定要给，不然会报错
+    init() {//swift的初始化方法 和 C++的构造函数名字并不相同
+        name = "婴儿"
+        age = 0
+    }
+    
+    //重载初始化方法 【重载：方法名相同、参数不同】
+    init(myAge: Int) {
+        age = myAge
+    }
+    
+    //析构方法
+    deinit {//注意 这里并没有 圆括号
+        print("当我被释放的时候会执行该方法")
+    }
+}
+
+var p2 = Person2()//此时 这里的Person2() 就会调用初始化方法
+print("我的年龄是：\(p2.age), 我的名字是:\(p2.name)")
+
+var p3 = Person2(myAge: 18)//调用带参数的初始化方法
+print("我的年龄是：\(p3.age), 我的名字是:\(p3.name)")
+
+var p4: Person2? = Person2()//为了能够让对象置空、所以定义了可选类型 此时【引用计数】为1、即p4的值指向了当前创建的对象
+p4 = nil//当p4=nil 时、此时【引用计数】为0、即 创建的对象没有任何一个变量指向它，所以它就释放了
+
+class MyPerson {
+    var age:Int
+    var name: String
+    
+    init() {
+        age = 0
+        name = "小孩"
+    }
+    
+    func play() {
+        print("我是MyPerson play")
+    }
+}
+
+class Teacher: MyPerson {
+    var obj:String
+    
+    override init() {
+        obj = "语文"
+        super.init()//调用父类的初始化 必须放在最后一行，要确保当前类的其他成员已经被初始化【有个疑问？如果当前成员的初始化依赖于父类成员的初始化怎么办？】
+    }
+    
+    override func play() {
+        print("我是Teacher play")
+    }
+}
+
+let t = MyPerson()
+t.play()
+
+//PS 结构体也可以像类一样使用，但是【结构体是深拷贝、类是浅拷贝】
+//   swift 的 类实例赋值是浅拷贝、即 不同的变量指向同一个对象(同一个内存地址)
+//   swift 的 结构体实例赋值是深拷贝 即 不同的变量指向不同的对象(即每次赋值的时候都会重新分配内存，不同的变量指向不同的内存地址)
+/*
+ 比如
+ var obj1 = Obj()
+ var obj2 = obj1   //这里的两个变量 实际上指向同一片内存
+ 
+ var structObj1 = StructObj()
+ var structObj2 = structObj1 //这里的两个变量 实际上指向不同的内存，这里的拷贝就会重新开辟内存空间
+ 
+ */
